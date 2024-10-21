54f8a69c621edc45f8aea0b1acec39ad4037f5ae
Abhishek Sanwal <abhisheksanwal745@gmail.com>
2024-10-21 11:11:16 +0530

    :sparkles: Added component for status display

---
+++ src/components/Status.js
@@ -0,0 +1,18 @@
+export default function Status({ items }) {
+  const total = items.length;
+  const totalPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((totalPacked / total) * 100);
+
+  return (
+    <footer className="stats">
+      <em>
+        {items.length === 0
+          ? "Start adding some items to your packing list🚀🚀"
+          : percentage === 100
+          ? "You got everything! Ready to go ✈️✈️"
+          : `💼💼You have ${total} items on your list, and you already packed
+         ${totalPacked}(${percentage}%)`}
+      </em>
+    </footer>
+  );
+}
